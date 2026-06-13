4f90d822a79d7af95f10ebb5203c8909b6129995
vanshgoyal000 <vanshgoyal9528@gmail.com>
2026-06-13 18:19:51 +0530

    test

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"DB/config"
 )
 
+// setting up the command line flags for host and port
 func setupFlags() {
 	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the dice server")
 	flag.IntVar(&config.Port, "port", 7379, "port for the dice server")
